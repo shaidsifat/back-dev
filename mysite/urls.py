8ec4a3b27cf57da40e94b5effe318aaf9167ba53
shaid sifat <42160933+shaidsifat@users.noreply.github.com>
2021-07-16 11:56:51 +0600

    Update urls.py

--- mysite/urls.py
+++ mysite/urls.py
@@ -22,6 +22,7 @@ from client import views
 
 from django.views.static import serve
 from django.conf.urls import url
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
