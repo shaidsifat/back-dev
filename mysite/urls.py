4e56122c7c5b810b05a11e170cf47c417e1e6759
shaid sifat <42160933+shaidsifat@users.noreply.github.com>
2021-07-16 12:03:35 +0600

    Update urls.py

--- mysite/urls.py
+++ mysite/urls.py
@@ -29,8 +29,8 @@ urlpatterns = [
     path('clent/',include('client.urls')),
     path('index/',views.index)
 
-    url(r'^media/(?P<path>.*)$', serve,{'document_root':  settings.MEDIA_ROOT}), 
-    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
+    #url(r'^media/(?P<path>.*)$', serve,{'document_root':  settings.MEDIA_ROOT}), 
+    #url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
 ]
 
 urlpatterns += staticfiles_urlpatterns()
